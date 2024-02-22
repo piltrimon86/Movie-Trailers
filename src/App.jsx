a802b90417568ec7e77b459423492b2265835a5f
Fernando Gómez <fernandogomezrecio@gmail.com>
2024-02-22 19:00:08 +0100

    Renderización del banner y el reproductor de vídeo

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import axios from 'axios'
-import YouTube from 'react-youtube'
 import './App.css'
+import { useState } from 'react'
+import YouTube from 'react-youtube'
 
-const App = () => {
+function App() {
     const API_URL = 'https://api.themoviedb.org/3'
-    const API_KEY = '31e525640d7e0c401602ee3129373d56'
+    const API_KEY = '4f5f43495afcc67e9553f6c684a82f84'
     const IMAGE_PATH = 'https://image.tmdb.org/t/p/original'
+
+    // endpoint para las imagenes
     const URL_IMAGE = 'https://image.tmdb.org/t/p/original'
 
-    // Variables de estado
-
+    // variables de estado
     const [movies, setMovies] = useState([])
     const [searchKey, setSearchKey] = useState('')
     const [trailer, setTrailer] = useState(null)
     const [movie, setMovie] = useState({ title: 'Loading Movies' })
     const [playing, setPlaying] = useState(false)
 
-    // Función para relaizar la petición por Get a la API
-
+    // funcion para realizar la peticion get a la api
     const fetchMovies = async (searchKey) => {
         const type = searchKey ? 'search' : 'discover'
         const {
@@ -32,10 +33,38 @@ const App = () => {
 
         setMovies(results)
         setMovie(results[0])
+
+        if (results.length) {
+            await fetchMovie(results[0].id)
+        }
     }
 
-    // Función para buscar películas
+    // funcion para la peticion de un solo objeto y mostrar en reproductor de videos
+    const fetchMovie = async (id) => {
+        const { data } = await axios.get(`${API_URL}/movie/${id}`, {
+            params: {
+                api_key: API_KEY,
+                append_to_response: 'videos',
+            },
+        })
 
+        if (data.videos && data.videos.results) {
+            const trailer = data.videos.results.find(
+                (vid) => vid.name === 'Official Trailer'
+            )
+            setTrailer(trailer ? trailer : data.videos.results[0])
+        }
+        setMovie(data)
+    }
+
+    const selectMovie = async (movie) => {
+        fetchMovie(movie.id)
+
+        setMovie(movie)
+        window.scrollTo(0, 0)
+    }
+
+    // funcion para buscar peliculas
     const searchMovies = (e) => {
         e.preventDefault()
         fetchMovies(searchKey)
@@ -47,8 +76,9 @@ const App = () => {
 
     return (
         <div>
-            <h2 className="text-center mt-5 mb-5">Trailer Movies</h2>
-            {/* BUSCADOR */}
+            <h2 className="text-center mt-5 mb-5">Trailer Popular Movies</h2>
+
+            {/* EL BUSCADOR */}
 
             <form className="container mb-4" onSubmit={searchMovies}>
                 <input
@@ -59,11 +89,80 @@ const App = () => {
                 <button className="btn btn-primary">Search</button>
             </form>
 
-            {/* CONTENDOR QUE MUESTRA LAS PELICULAS ACTUALES */}
+            {/* CONTENEDOR DEL BANNER Y DEL REPRODUCTOR DE VÍDEO */}
+
+            <div>
+                <main>
+                    {movie ? (
+                        <div
+                            className="viewtrailer"
+                            style={{
+                                backgroundImage: `url("${IMAGE_PATH}${movie.backdrop_path}")`,
+                            }}>
+                            {playing ? (
+                                <>
+                                    <YouTube
+                                        videoId={trailer.key}
+                                        className="reproductor container"
+                                        containerClassName={
+                                            'youtube-container amru'
+                                        }
+                                        opts={{
+                                            width: '100%',
+                                            height: '100%',
+                                            playerVars: {
+                                                autoplay: 1,
+                                                controls: 0,
+                                                cc_load_policy: 0,
+                                                fs: 0,
+                                                iv_load_policy: 0,
+                                                modestbranding: 0,
+                                                rel: 0,
+                                                showinfo: 0,
+                                            },
+                                        }}
+                                    />
+                                    <button
+                                        onClick={() => setPlaying(false)}
+                                        className="boton">
+                                        Close
+                                    </button>
+                                </>
+                            ) : (
+                                <div className="container">
+                                    <div className="">
+                                        {trailer ? (
+                                            <button
+                                                className="boton"
+                                                onClick={() => setPlaying(true)}
+                                                type="button">
+                                                Play Trailer
+                                            </button>
+                                        ) : (
+                                            'Sorry, no trailer available'
+                                        )}
+                                        <h1 className="text-white">
+                                            {movie.title}
+                                        </h1>
+                                        <p className="text-white">
+                                            {movie.overview}
+                                        </p>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    ) : null}
+                </main>
+            </div>
+
+            {/*CONTENEDOR PARA MOSTRAR LOS POSTER Y LAS PELÍCULAS EN LA PETICIÓN A LA API*/}
             <div className="container mt-3">
                 <div className="row">
                     {movies.map((movie) => (
-                        <div key={movie.id} className="col-md-4 mb-3">
+                        <div
+                            key={movie.id}
+                            className="col-md-4 mb-3"
+                            onClick={() => selectMovie(movie)}>
                             <img
                                 src={`${URL_IMAGE + movie.poster_path}`}
                                 alt=""
